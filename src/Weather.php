7c778754d4ea4326bc7fbaa20ed4f7102782d4cd
yuanhb <3345718878@qq.com>
2020-03-11 23:46:56 +0800

    版本更新

--- src/Weather.php
+++ src/Weather.php
@@ -35,39 +35,44 @@ class Weather
         $this->guzzleOptions = $options;
     }
 
-    public function getweather($city, $type = 'base', $format = 'json'){
 
+    public function getLiveWeather($city, $format = 'json')
+    {
+        return $this->getWeather($city, 'live', $format);
+    }
+
+    public function getForecastsWeather($city, $format = 'json')
+    {
+        return $this->getWeather($city, 'forecast', $format);
+    }
+
+
+    public function getWeather($city, string $type = 'live', string $format = 'json')
+    {
         $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
-
-        // 1. 对 `$format` 与 `$type` 参数进行检查，不在范围内的抛出异常。
-        if (!\in_array($format, ['xml', 'json'])) {
-            throw new InvalidArgumentException('Invalid response format: '. $format);
+        $types = [
+            'live' => 'base',
+            'forecast' => 'all',
+        ];
+        if (!\in_array(\strtolower($format), ['xml', 'json'])) {
+            throw new InvalidArgumentException('Invalid response format: '.$format);
         }
-
-        if (!\in_array(\strtolower($type), ['base', 'all'])) {
-            throw new InvalidArgumentException('Invalid type value(base/all): '.$type);
+        if (!\array_key_exists(\strtolower($type), $types)) {
+            throw new InvalidArgumentException('Invalid type value(live/forecast): '.$type);
         }
-
-        // 2. 封装 query 参数，并对空值进行过滤。
         $query = array_filter([
             'key' => $this->key,
             'city' => $city,
             'output' => $format,
-            'extensions' => $type,
+            'extensions' => $types[\strtolower($type)],
         ]);
 
         try {
-            // 3. 调用 getHttpClient 获取实例，并调用该实例的 `get` 方法，
-            // 传递参数为两个：$url、['query' => $query]，
             $response = $this->getHttpClient()->get($url, [
                 'query' => $query,
             ])->getBody()->getContents();
-            // 4. 返回值根据 $format 返回不同的格式，
-            // 当 $format 为 json 时，返回数组格式，否则为 xml。
-            return $format === 'json' ? \json_decode($response, true) : $response;
+            return 'json' === $format ? \json_decode($response, true) : $response;
         } catch (\Exception $e) {
-            // 5. 当调用出现异常时捕获并抛出，消息为捕获到的异常消息，
-            // 并将调用异常作为 $previousException 传入。
             throw new HttpException($e->getMessage(), $e->getCode(), $e);
         }
     }
